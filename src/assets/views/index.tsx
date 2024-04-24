3f0e508f6e0216de88c99812bde5bf0539111bd5
fgpsms <87663477+fgpsms@users.noreply.github.com>
2024-04-24 17:09:24 -0400

    preventDefault

--- src/assets/views/index.tsx
+++ src/assets/views/index.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+
+interface Todo {
+  todo: string;
+}
 export function Todos() {
   const tarefas = [
     {todo: "acordar" },
@@ -7,15 +12,36 @@ export function Todos() {
     {todo: "fazer o almoço" },
   ];
 
-  return (
+const [todos, setTodos] = useState<Todo[]>([]);
+const [form, setForm] = useState<Todo>({ todo: "" });
+console.log("🚀 ~ Todos `form:", form.todo);
+
+const handleInputChange = (event) => {
+  const { name, value } = event.target;
+  setForm({  ... form, [name]: value});
+};
+const handleSubmit = (event) => {
+  event.preventDefault();
+  if (form.todo) {
+    setTodos([form, ... todos]);
+    setForm
+  }
+}
+
+return (
   <div>
     <h1>Todos de Prog  Web</h1>
-    <form>
-      <input type="text"/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+      name="todo" 
+      value={form.todo}
+      placeholder="Sua melhor tarefa"
+      onChange={handleInputChange}
+      />
       <button type="submit"> Adicionar Tarefa </button>
     </form>
     <ul>
-      {tarefas.map((tarefa, index) => (
+      {todos.map((tarefa, index) => (
         <li key={index}>{tarefa.todo}</li>
       ))}
     </ul>
